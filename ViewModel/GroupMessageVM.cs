27538172cf5598a70847c4663104ce192e561b8a
basig120498@gmail.com <basig120498@gmail.com>
2022-07-18 11:52:37 +0800

    with select/deselectall message/final

---
+++ ViewModel/GroupMessageVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TryLogin.Models;
+
+namespace TryLogin.ViewModel
+{
+    public class GroupMessageVM
+    {
+        public IEnumerable<Draft> DraftList { get; set; }
+        public Draft Draft { get; set; }
+
+        public IEnumerable<CgppOffice> OfficeList { get; set; }
+        public CgppOffice Office { get; set; }
+        public IEnumerable<CgppPhonebook> PhonebookList { get; set; }
+        public CgppPhonebook Phonebook { get; set; }
+    }
+}
